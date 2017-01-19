543c6965e6c1580c4f39e65436be4fae603522fe
wwmin <wwei.min@163.com>
2017-01-19 13:34:11 +0800

    添加ejs模板引擎,并用模板差分法拆分主页面

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var path=require('path');
 var express = require('express');
 var app = express();
+
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
+
+app.set('views',path.join(__dirname,'views'));//设置存放模板文件的目录
+app.set('view engine','ejs');//设置模板引擎为 ejs
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name', function (req, res) {
-  res.send('hello,' + req.params.name);
+  res.render('users', {
+    name: req.params.name
+  });
 });
 module.exports = router;
