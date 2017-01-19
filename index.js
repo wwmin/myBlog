fbb8455de8501503f0f77fe3577c38831fe38178
wwmin <wwei.min@163.com>
2017-01-19 23:05:47 +0800

    开发完成

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var flash = require('connect-flash');
 var config = require('config-lite');
 var routes = require('./routes');
 var pkg = require('./package');
-
+var winston = require('winston');
+var expressWinston = require('express-winston');
 var app = express();
 
 // 设置模板目录
@@ -29,6 +30,11 @@ app.use(session({
 }));
 // flash 中间价，用来显示通知
 app.use(flash());
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'),// 上传文件目录
+  keepExtensions: true// 保留后缀
+}));
 // 设置模板全局常量
 app.locals.blog = {
   title: pkg.name,
@@ -42,9 +48,38 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error').toString();
   next();
 });
+// 正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new (winston.transports.Console)({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success.log'
+    })
+  ]
+}));
 // 路由
 routes(app);
-
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ]
+}));
+// error page
+app.use(function (err, req, res, next) {
+  res.render('error', {
+    error: err
+  });
+});
 // 监听端口，启动程序
 app.listen(config.port, function () {
   console.log(`${pkg.name} listening on port ${config.port}`);
